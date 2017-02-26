99e0067ccc0058a2d66a086215e04b9806790f1d
Janos Guljas <janos@resenje.org>
2017-02-27 00:13:05 +0100

    Add FinalHandler and FinalHandlerFunc functions

--- chain_test.go
+++ chain_test.go
@@ -52,3 +52,59 @@ func TestChain(t *testing.T) {
 		t.Errorf("expected body %q, got %q", "012", string(b))
 	}
 }
+
+func TestFinalHandler(t *testing.T) {
+	handlers := []func(http.Handler) http.Handler{
+		func(h http.Handler) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte("0"))
+				if h != nil {
+					h.ServeHTTP(w, r)
+				}
+			})
+		},
+		FinalHandler(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("1"))
+		})),
+	}
+
+	r := httptest.NewRequest("", "/", nil)
+	w := httptest.NewRecorder()
+	ChainHandlers(handlers...).ServeHTTP(w, r)
+
+	b, err := ioutil.ReadAll(w.Result().Body)
+	if err != nil {
+		t.Error(err)
+	}
+	if string(b) != "01" {
+		t.Errorf("expected body %q, got %q", "01", string(b))
+	}
+}
+
+func TestFinalHandlerFunc(t *testing.T) {
+	handlers := []func(http.Handler) http.Handler{
+		func(h http.Handler) http.Handler {
+			return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte("0"))
+				if h != nil {
+					h.ServeHTTP(w, r)
+				}
+			})
+		},
+		FinalHandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("1"))
+		}),
+	}
+
+	r := httptest.NewRequest("", "/", nil)
+	w := httptest.NewRecorder()
+	ChainHandlers(handlers...).ServeHTTP(w, r)
+
+	b, err := ioutil.ReadAll(w.Result().Body)
+	if err != nil {
+		t.Error(err)
+	}
+	if string(b) != "01" {
+		t.Errorf("expected body %q, got %q", "01", string(b))
+	}
+}
