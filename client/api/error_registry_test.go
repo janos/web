b36a165442626e984c16a39f0745a1d061673144
Janos Guljas <janos@resenje.org>
2017-04-01 18:58:36 +0200

    Add more apiClient.ErrorRegistry tests

--- client/api/error_registry_test.go
+++ client/api/error_registry_test.go
@@ -10,26 +10,117 @@ import (
 	"testing"
 )
 
-var errTest = errors.New("test error")
+var (
+	errTest        = errors.New("test error")
+	errHandlerTest = errors.New("test handler error")
+)
+
+func errHandler(body []byte) error {
+	return errHandlerTest
+}
 
 func TestMapErrorRegistry(t *testing.T) {
-	r := NewMapErrorRegistry(nil)
+	r := NewMapErrorRegistry(nil, nil)
 	code := 1000
 	if err := r.AddError(code, errTest); err != nil {
-		t.Error(err.Error())
+		t.Error(err)
 	}
 	if err := r.Error(code); err != errTest {
 		t.Errorf("expected error %v, got %v", errTest, err)
 	}
+	code = 1001
+	errTestMessage, err := r.AddMessageError(code, "error message")
+	if err != nil {
+		t.Error(err)
+	}
+	if err := r.Error(code); err != errTestMessage {
+		t.Errorf("expected error %v, got %v", errTestMessage, err)
+	}
+	code = 1002
+	errTestMessage2 := r.MustAddMessageError(code, "error message2")
+	if err := r.Error(code); err != errTestMessage2 {
+		t.Errorf("expected error %v, got %v", errTestMessage2, err)
+	}
+	code = 1003
+	if err := r.AddHandler(code, errHandler); err != nil {
+		t.Error(err)
+	}
+	if handler := r.Handler(code); handler != nil {
+		err := handler(nil)
+		if err != errHandlerTest {
+			t.Errorf("expected error %v, got %v", errHandlerTest, err)
+		}
+	}
 }
 
 func TestMapErrorRegistryErrorAlreadyRegistered(t *testing.T) {
-	r := NewMapErrorRegistry(nil)
+	r := NewMapErrorRegistry(nil, nil)
 	code := 1000
 	if err := r.AddError(code, errTest); err != nil {
-		t.Error(err.Error())
+		t.Error(err)
 	}
 	if err := r.AddError(code, errTest); err != ErrErrorAlreadyRegistered {
 		t.Errorf("expected error %v, got %v", ErrErrorAlreadyRegistered, err)
 	}
+	if _, err := r.AddMessageError(code, "message"); err != ErrErrorAlreadyRegistered {
+		t.Errorf("expected error %v, got %v", ErrErrorAlreadyRegistered, err)
+	}
+	if err := r.AddHandler(code, errHandler); err != ErrErrorAlreadyRegistered {
+		t.Errorf("expected error %v, got %v", ErrErrorAlreadyRegistered, err)
+	}
+	code = 2000
+	if err := r.AddHandler(code, errHandler); err != nil {
+		t.Error(err)
+	}
+	if err := r.AddHandler(code, errHandler); err != ErrErrorAlreadyRegistered {
+		t.Errorf("expected error %v, got %v", ErrErrorAlreadyRegistered, err)
+	}
+	if err := r.AddError(code, errTest); err != ErrErrorAlreadyRegistered {
+		t.Errorf("expected error %v, got %v", ErrErrorAlreadyRegistered, err)
+	}
+	if _, err := r.AddMessageError(code, "message"); err != ErrErrorAlreadyRegistered {
+		t.Errorf("expected error %v, got %v", ErrErrorAlreadyRegistered, err)
+	}
+}
+
+func TestMapErrorRegistryMustAddErrorPanic(t *testing.T) {
+	r := NewMapErrorRegistry(nil, nil)
+	code := 1000
+	if err := r.AddError(code, errTest); err != nil {
+		t.Error(err)
+	}
+	defer func() {
+		if err := recover(); err != ErrErrorAlreadyRegistered {
+			t.Errorf("expected error %v, got %v", ErrErrorAlreadyRegistered, err)
+		}
+	}()
+	r.MustAddError(code, errTest)
+}
+
+func TestMapErrorRegistryMustAddMessageErrorPanic(t *testing.T) {
+	r := NewMapErrorRegistry(nil, nil)
+	code := 1000
+	if err := r.AddError(code, errTest); err != nil {
+		t.Error(err)
+	}
+	defer func() {
+		if err := recover(); err != ErrErrorAlreadyRegistered {
+			t.Errorf("expected error %v, got %v", ErrErrorAlreadyRegistered, err)
+		}
+	}()
+	r.MustAddMessageError(code, "message")
+}
+
+func TestMapErrorRegistryMustAddHandlerPanic(t *testing.T) {
+	r := NewMapErrorRegistry(nil, nil)
+	code := 1000
+	if err := r.AddError(code, errTest); err != nil {
+		t.Error(err)
+	}
+	defer func() {
+		if err := recover(); err != ErrErrorAlreadyRegistered {
+			t.Errorf("expected error %v, got %v", ErrErrorAlreadyRegistered, err)
+		}
+	}()
+	r.MustAddHandler(code, errHandler)
 }
